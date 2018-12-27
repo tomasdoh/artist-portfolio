8ae047c889873294acc458daad191d851045920a
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-27 11:23:17 +0000

    Installed Typography.js; created 'About' page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+<div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    {" "}
+    
+<h1>Willie Doherty</h1>
+<h2> Artist </h2>
+</div>
+)
