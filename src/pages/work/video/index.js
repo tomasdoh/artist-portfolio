4f0c325d63906357026f6c7a97d021bd01b95bb4
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-10 16:28:12 +0000

    Removed reference to Helmet in video index page

--- src/pages/work/video/index.js
+++ src/pages/work/video/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import { Helmet } from "react-helmet"
 import Layout from "../../../components/layout"
 import Menu from "../../../components/workMenu"
 import Img from "gatsby-image"
